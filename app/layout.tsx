43c49fef2ca01d6c27b51e5cde3e494e71d09523
Francis Padero <francis.padero@1.ustp.edu.ph>
2026-03-21 17:15:38 +0800

    chore: random package changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,43 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono, Inter } from "next/font/google"
+import "./globals.css"
+import { cn } from "@/lib/utils"
 
 // components
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { ThemeProvider } from "@/components/theme-provider"
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "BrewScholar",
   description:
     "Pour over opportunities and brew up your brightest future with BrewScholar",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", inter.variable)}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1E1E1E] text-white`}
-      >
-                <ThemeProvider>
+    <html lang="en" className={cn(
+      "antialiased",geistMono.variable, geistSans.variable,"font-sans", inter.variable)}>
+      <body>
+        <ThemeProvider>
           <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
